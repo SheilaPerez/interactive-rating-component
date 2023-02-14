74728fdaf1d8d0fafc7114936d9e509bea9f2fc4
SheilaPerez <sheilapg26@gmail.com>
2023-02-14 15:29:22 +0100

    fixed desktop

--- src/Rating/Rating.tsx
+++ src/Rating/Rating.tsx
@@ -23,7 +23,7 @@ const Rating = () => {
                     <Star/>           
                     </div>
                     <h1 className={styles.question}>How did we do?</h1>
-                    <h1 className={styles.description}>Please let us know how we did width your support request. All feedback is appreciated to help us improve our offering!</h1>
+                    <p className={styles.description}>Please let us know how we did width your support request. All feedback is appreciated to help us improve our offering!</p>
                     <Numbers numberSelected={numSelected}></Numbers>
                     <SubmitBtn submitBtn={submitBtnClicked}></SubmitBtn>
                 </div>    
@@ -32,7 +32,7 @@ const Rating = () => {
                     <Thankyou></Thankyou>
                     <h2 className={styles.whatSelected}>You selected {number} out of 5</h2>
                     <h1 className={styles.thanks}>Thank you!</h1>
-                    <p className={styles.description}>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+                    <p className={styles.thanksDescription}>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
                 </div>
                 }
             
